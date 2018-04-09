9c6cab8617ea9637f6516ff06884ce1cb06e85d8
kientc6811 <kientc6811@co-well.com.vn>
2018-04-09 16:51:49 +0700

    db

--- pizza.sql
+++ pizza.sql
@@ -10,15 +10,15 @@ Target Server Type    : MYSQL
 Target Server Version : 50624
 File Encoding         : 65001
 
-Date: 2018-04-05 10:58:17
+Date: 2018-04-09 16:51:04
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
--- Table structure for `bannes`
+-- Table structure for `banner`
 -- ----------------------------
-DROP TABLE IF EXISTS `bannes`;
-CREATE TABLE `bannes` (
+DROP TABLE IF EXISTS `banner`;
+CREATE TABLE `banner` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE `bannes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of bannes
+-- Records of banner
 -- ----------------------------
 
 -- ----------------------------
@@ -44,11 +44,12 @@ CREATE TABLE `categories` (
   `created_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of categories
 -- ----------------------------
+INSERT INTO categories VALUES ('4', 'a a b a', 'a-a-b-a', '1', '2018-04-06 10:29:17', '2018-04-06 03:29:17');
 
 -- ----------------------------
 -- Table structure for `comment`
@@ -161,14 +162,19 @@ CREATE TABLE `products` (
   `image` varchar(255) NOT NULL,
   `price` double DEFAULT NULL,
   `status` tinyint(4) NOT NULL,
+  `sale` tinyint(4) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `category_id` int(11) DEFAULT NULL,
+  `user_id_create` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of products
 -- ----------------------------
+INSERT INTO products VALUES ('2', 'test ', 'test', '<p>test</p>', 'http://store-test.local/source/category.png', '5678568', '0', '10', '2018-04-06 16:56:12', '2018-04-06 09:56:12', '3', '1');
+INSERT INTO products VALUES ('3', 'aaaa', 'aaaa', '<p>test</p>', 'http://store-test.local/source/Untitled.png', '5678568', '1', '10', '2018-04-06 17:17:04', '2018-04-06 17:17:04', '4', '1');
 
 -- ----------------------------
 -- Table structure for `rate_product`
@@ -201,11 +207,13 @@ CREATE TABLE `role` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of role
 -- ----------------------------
+INSERT INTO role VALUES ('1', 'admin', 'admin', '1', '1', '', '', '2018-04-09 10:25:04', null);
+INSERT INTO role VALUES ('2', 'member', 'member', '2', '1', '', '', '2018-04-09 10:25:43', null);
 
 -- ----------------------------
 -- Table structure for `users`
@@ -219,19 +227,20 @@ CREATE TABLE `users` (
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `last_login` datetime DEFAULT NULL,
   `active` tinyint(4) NOT NULL DEFAULT '1',
-  `is_admin` tinyint(4) NOT NULL DEFAULT '0',
   `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
+  `is_admin` tinyint(4) DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO users VALUES ('1', 'Admin', 'Admin', 'kienkienutc95@gmail.com', '$2y$10$rlK4DHEnZtPhJFcFFbgj5ezqWfQnvYAgdZPvLj81StpgNLor6TnSW', null, '1', '1', 'XQrxQMyFy1ocRtvD3mbRhXWieS67bLkqptmdAfhOfNJBU8b5KXNJMDeGoE9r', null, '2017-12-25 06:38:25');
-INSERT INTO users VALUES ('2', 'kientran', 'Trần Chung Kiên', 'admin@gmail.com', '$2y$10$XuQUgBKPYiwb8e6OCbT8ve7C55n0ndbsqWowaEg9du4ll/FG2nx0W', null, '0', '0', 'hRTlY6VaWMNtUx7G4N6KTFLhJJPLZ7Tl10YoYojsyWYFpNJ3o6kBEm2CJu5m', '2017-12-25 06:36:21', '2017-12-25 08:51:43');
+INSERT INTO users VALUES ('1', 'Admin', 'Admin', 'kienkienutc95@gmail.com', '$2y$10$rlK4DHEnZtPhJFcFFbgj5ezqWfQnvYAgdZPvLj81StpgNLor6TnSW', null, '0', 'ITar89cOhsZMs6B7EPtSWb63dg8D1nOJRoCTZqrofkX0Km9owpi5Uk0VKfD6', null, '2018-04-09 09:49:53', '1', '');
+INSERT INTO users VALUES ('2', 'kientran', 'Trần Chung Kiên', 'admin@gmail.com', '$2y$10$TE7NFzcurLq6MG8j8dOPDu016O7j91YXFo5QLxLm.HPz17yjyceEe', null, '0', 'ntSTCOl83rBaor55UbOHMWgotKFlcGoY0J9wfQFEJDxyIjdz916LAXHpZg5D', '2017-12-25 06:36:21', '2018-04-09 09:50:00', null, 'http://store-test.local/source/Untitled.png');
 
 -- ----------------------------
 -- Table structure for `user_role`
@@ -240,12 +249,13 @@ DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role` (
   `user_id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL,
-  `active` tinyint(4) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`,`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of user_role
 -- ----------------------------
-INSERT INTO user_role VALUES ('2', '2', '0', '2017-12-25 06:36:21', '2017-12-25 06:36:21');
+INSERT INTO user_role VALUES ('1', '1', '2017-12-25 06:36:21', '2018-04-09 09:49:30');
+INSERT INTO user_role VALUES ('2', '2', null, '2018-04-09 09:49:45');
