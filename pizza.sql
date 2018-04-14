f7c5421128855642f0145ebfd983706a30723e7d
kientranutc <kienkienutc95@gmail.com>
2018-04-14 17:15:19 +0700

    db news

--- pizza.sql
+++ pizza.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2018-04-13 16:59:39
+Date: 2018-04-14 17:14:46
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -64,14 +64,20 @@ CREATE TABLE `comment` (
   `user_id` int(11) DEFAULT NULL,
   `content` text NOT NULL,
   `status` tinyint(4) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of comment
 -- ----------------------------
+INSERT INTO comment VALUES ('1', '4', '2', 'test', '1', '2018-04-14 04:09:27', '2018-04-14 04:09:27');
+INSERT INTO comment VALUES ('2', '4', '1', 'test-user', '1', '2018-04-14 04:19:39', '2018-04-14 04:19:39');
+INSERT INTO comment VALUES ('3', '4', '1', 'testaaa', '1', '2018-04-14 04:21:48', '2018-04-14 04:21:48');
+INSERT INTO comment VALUES ('4', '3', '1', 'test', '1', '2018-04-14 11:22:13', '2018-04-14 04:22:13');
+INSERT INTO comment VALUES ('5', '3', '1', 'test', '1', '2018-04-14 04:33:47', '2018-04-14 04:33:47');
+INSERT INTO comment VALUES ('6', '3', '1', 'testafasga', '1', '2018-04-14 11:36:31', '2018-04-14 11:36:31');
 
 -- ----------------------------
 -- Table structure for `customer`
@@ -89,13 +95,16 @@ CREATE TABLE `customer` (
   `created_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of customer
 -- ----------------------------
-INSERT INTO customer VALUES ('1', 'test', 'test', 'kienkienutc95@gamil.com', 'áadfdsgh', '1', '0964953028', 'Hà nội', '2018-04-12 16:36:47', '2018-04-12 16:36:47');
-INSERT INTO customer VALUES ('2', 'kientran', null, 'kienkienutc95@gmail.com', 'e6e061838856bf47e1de730719fb2609', null, '0964953029', null, '2018-04-12 09:36:56', '2018-04-12 09:36:56');
+INSERT INTO customer VALUES ('1', 'test', 'test', 'kienkienutc95@gamil.com', 'e6e061838856bf47e1de730719fb2609', '1', '0964953028', 'Hà nội', '2018-04-14 11:18:34', '2018-04-14 11:18:34');
+INSERT INTO customer VALUES ('2', 'kientran', 'Trần Chung Kiên', 'kienkienutc95@gmail.com', 'e6e061838856bf47e1de730719fb2609', null, '0964953029', 'Hà nội', '2018-04-14 15:20:27', '2018-04-14 15:20:27');
+INSERT INTO customer VALUES ('3', 'mr.bean', 'Test1', 'kienkienutc95@gmail.coms', 'e6e061838856bf47e1de730719fb2609', null, '0964953027', 'Hà nội', '2018-04-14 15:20:34', '2018-04-14 15:20:34');
+INSERT INTO customer VALUES ('4', null, 'Trần Chung Kiên', null, null, null, null, 'Hà nội ', '2018-04-14 16:10:34', '2018-04-14 16:10:34');
+INSERT INTO customer VALUES ('5', null, 'Trần Chung Kiên', 'kienkienutc95@gmail.com', null, null, '0964953029', 'Hà nội ', '2018-04-14 16:15:46', '2018-04-14 16:15:46');
 
 -- ----------------------------
 -- Table structure for `news`
@@ -119,10 +128,10 @@ CREATE TABLE `news` (
 -- ----------------------------
 -- Records of news
 -- ----------------------------
-INSERT INTO news VALUES ('2', 'Test1-1', '1', 'http://store-test.local/source/sale1.jpg', 'test1-1', '<p>test-description2</p>', '1', '2018-04-13 15:41:26', '2018-04-13 08:41:26', 'Admin', 'Admin');
-INSERT INTO news VALUES ('3', 'Tests11', '1', 'http://store-test.local/source/sale2.jpg', 'tests11', '', '1', '2018-04-13 08:35:21', '2018-04-13 08:35:21', 'Admin', null);
-INSERT INTO news VALUES ('4', 'test2', '1', 'http://store-test.local/source/sale2_1.jpg', 'test2', '', '1', '2018-04-13 08:35:45', '2018-04-13 08:35:45', 'Admin', null);
-INSERT INTO news VALUES ('5', 'Test1-13', '1', 'http://store-test.local/source/sale3.png', 'test1-13', '<p>test</p>', '1', '2018-04-13 08:36:10', '2018-04-13 08:36:10', 'Admin', null);
+INSERT INTO news VALUES ('2', 'Test1-1', '1', 'http://pizza.local:8080/source/sale1.jpg', 'test1-1', '<p>test-description2</p>', '1', '2018-04-14 08:22:24', '2018-04-14 01:22:24', 'Admin', 'Admin');
+INSERT INTO news VALUES ('3', 'Tests11', '1', 'http://pizza.local:8080/source/sale2.jpg', 'tests11', '', '1', '2018-04-14 08:22:12', '2018-04-14 01:22:12', 'Admin', 'Admin');
+INSERT INTO news VALUES ('4', 'test2', '1', 'http://pizza.local:8080/source/sale2.jpg', 'test2', '', '1', '2018-04-14 08:22:01', '2018-04-14 01:22:01', 'Admin', 'Admin');
+INSERT INTO news VALUES ('5', 'Test1-13', '1', 'http://pizza.local:8080/source/sale3.png', 'test1-13', '<p>test</p>', '1', '2018-04-14 08:21:50', '2018-04-14 01:21:50', 'Admin', 'Admin');
 
 -- ----------------------------
 -- Table structure for `orders`
@@ -132,7 +141,7 @@ CREATE TABLE `orders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date_order` datetime DEFAULT NULL,
   `note` text,
-  `total` double NOT NULL,
+  `total` float(14,0) NOT NULL,
   `customer_id` int(11) NOT NULL,
   `status` tinyint(4) DEFAULT '0',
   `user_update` varchar(255) DEFAULT NULL,
@@ -144,7 +153,7 @@ CREATE TABLE `orders` (
 -- ----------------------------
 -- Records of orders
 -- ----------------------------
-INSERT INTO orders VALUES ('1', '2018-04-11 13:23:00', 'test', '1000', '1', '0', 'Admin', '2018-04-11 15:03:12', '2018-04-11 08:03:12');
+INSERT INTO orders VALUES ('1', '2018-04-14 16:15:47', 'giao sơm', '316000', '5', '0', null, '2018-04-14 16:15:47', '2018-04-14 16:15:47');
 
 -- ----------------------------
 -- Table structure for `order_detail`
@@ -164,8 +173,8 @@ CREATE TABLE `order_detail` (
 -- ----------------------------
 -- Records of order_detail
 -- ----------------------------
-INSERT INTO order_detail VALUES ('1', '2', '60000', '2', '10', '2018-04-11 14:33:45', '2018-04-11 14:33:45');
-INSERT INTO order_detail VALUES ('1', '3', '5000', '1', null, '2018-04-11 13:24:19', '2018-04-11 13:24:19');
+INSERT INTO order_detail VALUES ('1', '3', '120000', '2', '10', '2018-04-14 16:15:47', '2018-04-14 16:15:47');
+INSERT INTO order_detail VALUES ('1', '4', '100000', '1', '0', '2018-04-14 16:15:47', '2018-04-14 16:15:47');
 
 -- ----------------------------
 -- Table structure for `products`
@@ -190,10 +199,10 @@ CREATE TABLE `products` (
 -- ----------------------------
 -- Records of products
 -- ----------------------------
-INSERT INTO products VALUES ('2', 'test ', 'test', '<p>test</p>', 'http://store-test.local/source/cake2.png', '5678568', '1', '10', '2018-04-12 16:50:29', '2018-04-12 09:50:29', '4', '1');
-INSERT INTO products VALUES ('3', 'Pizza', 'pizza', '<p>test</p>', 'http://store-test.local/source/cake2.png', '5678568', '1', '10', '2018-04-12 14:56:33', '2018-04-12 07:56:33', '4', '1');
-INSERT INTO products VALUES ('4', 'Pizza phô mai', 'pizza-pho-mai', '<p>test</p>', 'http://store-test.local/source/cake2.png', '100000', '1', '0', '2018-04-12 15:03:06', '2018-04-12 15:03:06', '4', '1');
-INSERT INTO products VALUES ('5', 'pizza test', 'pizza-test', '<p>test</p>', 'http://store-test.local/source/cake1.png', '100000', '1', '0', '2018-04-13 10:40:02', '2018-04-13 10:40:02', '4', '1');
+INSERT INTO products VALUES ('2', 'test ', 'test', '<p>test</p>', 'http://pizza.local:8080/source/cake2.png', '200000', '1', '10', '2018-04-14 08:21:33', '2018-04-14 01:21:33', '4', '1');
+INSERT INTO products VALUES ('3', 'Pizza', 'pizza', '<p>test</p>', 'http://pizza.local:8080/source/cake2.png', '120000', '1', '10', '2018-04-14 08:21:23', '2018-04-14 01:21:23', '4', '1');
+INSERT INTO products VALUES ('4', 'Pizza phô mai', 'pizza-pho-mai', '<p>test</p>', 'http://pizza.local:8080/source/cake1.png', '100000', '1', '0', '2018-04-14 08:21:11', '2018-04-14 01:21:11', '4', '1');
+INSERT INTO products VALUES ('5', 'pizza test', 'pizza-test', '<p>aaaac aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>', 'http://pizza.local:8080/source/cake2.png', '100000', '1', '0', '2018-04-14 09:25:10', '2018-04-14 02:25:10', '4', '1');
 
 -- ----------------------------
 -- Table structure for `rate_product`
@@ -202,23 +211,35 @@ DROP TABLE IF EXISTS `rate_product`;
 CREATE TABLE `rate_product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_id` int(11) DEFAULT NULL,
-  `ip_address` varchar(255) DEFAULT NULL,
   `rate_number` tinyint(4) DEFAULT NULL,
-  `date_create` date DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `product_id` (`product_id`),
   CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of rate_product
 -- ----------------------------
-INSERT INTO rate_product VALUES ('1', '2', null, '2', '2018-04-13');
-INSERT INTO rate_product VALUES ('2', '3', null, '4', '2018-04-13');
-INSERT INTO rate_product VALUES ('3', '2', null, '3', '2018-04-13');
-INSERT INTO rate_product VALUES ('4', '4', null, '1', '2018-04-13');
-INSERT INTO rate_product VALUES ('5', '4', null, '4', '2018-04-13');
-INSERT INTO rate_product VALUES ('6', '5', null, '1', '2018-04-13');
+INSERT INTO rate_product VALUES ('1', '2', '2', null, null);
+INSERT INTO rate_product VALUES ('2', '3', '4', null, null);
+INSERT INTO rate_product VALUES ('3', '2', '3', null, null);
+INSERT INTO rate_product VALUES ('4', '4', '1', null, null);
+INSERT INTO rate_product VALUES ('5', '4', '4', null, null);
+INSERT INTO rate_product VALUES ('6', '5', '1', null, null);
+INSERT INTO rate_product VALUES ('7', '4', '5', '2018-04-14 01:24:00', '2018-04-14 01:24:00');
+INSERT INTO rate_product VALUES ('8', '4', '4', '2018-04-14 01:28:38', '2018-04-14 01:28:38');
+INSERT INTO rate_product VALUES ('9', '2', '5', '2018-04-14 01:31:16', '2018-04-14 01:31:16');
+INSERT INTO rate_product VALUES ('10', '2', '5', '2018-04-14 01:31:21', '2018-04-14 01:31:21');
+INSERT INTO rate_product VALUES ('11', '4', '4', '2018-04-14 01:34:42', '2018-04-14 01:34:42');
+INSERT INTO rate_product VALUES ('12', '3', '4', '2018-04-14 01:35:08', '2018-04-14 01:35:08');
+INSERT INTO rate_product VALUES ('13', '2', '5', '2018-04-14 01:35:13', '2018-04-14 01:35:13');
+INSERT INTO rate_product VALUES ('14', '2', '4', '2018-04-14 01:42:18', '2018-04-14 01:42:18');
+INSERT INTO rate_product VALUES ('15', '3', '4', '2018-04-14 02:11:50', '2018-04-14 02:11:50');
+INSERT INTO rate_product VALUES ('16', '4', '2', '2018-04-14 02:50:55', '2018-04-14 02:50:55');
+INSERT INTO rate_product VALUES ('17', '3', '4', '2018-04-14 03:49:34', '2018-04-14 03:49:34');
+INSERT INTO rate_product VALUES ('18', null, null, '2018-04-14 16:54:58', '2018-04-14 16:54:58');
 
 -- ----------------------------
 -- Table structure for `role`
